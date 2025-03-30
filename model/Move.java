092acecfb5d32300280066219f70be83307c8876
DominikSain <dominiksain96@gmail.com>
2025-03-30 12:06:00 +0200

    I did move and vehicle class

--- model/Move.java
+++ model/Move.java
@@ -1,5 +1,21 @@
 package model;
 
+/** @author Dominik Sain */
 public class Move {
-    
+    private char vehicleId;
+    private Direction direction;
+
+    public Move(char vehicleId, Direction direction) {
+        this.vehicleId = vehicleId;
+        this.direction = direction;
+    }
+
+    public char getVehicleId() {
+        return vehicleId;
+    }
+
+    public Direction getDirection() {
+        return direction;
+    }
 }
+
--- model/Vehicle.java
+++ model/Vehicle.java
@@ -1,5 +1,52 @@
 package model;
 
+/** @author Dominik Sain */
 public class Vehicle {
-    
+    private char id;
+    private Position back;
+    private Position front;
+    private boolean isHorizontal;
+
+    public Vehicle(char id, Position back, Position front) {
+        this.id = id;
+        this.back = back;
+        this.front = front;
+        this.isHorizontal = (back.row == front.row); // True if the vehicle is horizontal
+    }
+
+    public char getId() {
+        return id;
+    }
+
+    public Position getBack() {
+        return back;
+    }
+
+    public Position getFront() {
+        return front;
+    }
+
+    public boolean isHorizontal() {
+        return isHorizontal;
+    }
+
+    public void move(Direction direction) {
+        if (isHorizontal) { //If vehicle is horizontal we only move columns left or right
+            if (direction == Direction.LEFT) {
+                back = new Position(back.row, back.col - 1);
+                front = new Position(front.row, front.col - 1);
+            } else if (direction == Direction.RIGHT) {
+                back = new Position(back.row, back.col + 1);
+                front = new Position(front.row, front.col + 1);
+            }
+        } else { // if it is vertical we move it by changing rows up or down
+            if (direction == Direction.UP) {
+                back = new Position(back.row - 1, back.col);
+                front = new Position(front.row - 1, front.col);
+            } else if (direction == Direction.DOWN) {
+                back = new Position(back.row + 1, back.col);
+                front = new Position(front.row + 1, front.col);
+            }
+        }
+    }
 }
